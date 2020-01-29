472980b514ad3b4e6822c43948ead5237fbd6cd0
wedojava <wedojava@gmail.com>
2020-01-30 00:33:44 +0800

    practice 03

---
+++ Ch3/practice_03.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\007\aStartled by the sudden sound, Sally shouted,\n");
+    printf("\"By the Great Pumpkin, what was that!\"");
+
+    return 0;
+}
