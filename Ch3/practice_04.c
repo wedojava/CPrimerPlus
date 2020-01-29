86fe3a20d66d491f2aaa6359d5054d9db94e20aa
wedojava <wedojava@gmail.com>
2020-01-30 00:42:30 +0800

    practice 04

---
+++ Ch3/practice_04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    float f;
+    printf("Enter a floating-point value: ");
+    scanf("%f", &f);
+    printf("fixed-point notation: %f\n", f);
+    printf("exponential notation: %e\n", f);
+    printf("p notation: %a\n", f);
+
+    return 0;
+}
