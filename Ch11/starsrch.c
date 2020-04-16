93f8aed9c0c35e2031ec5b957fead08265cf4efc
wedojava <wedojava@gmail.com>
2020-04-16 21:40:00 +0800

    study at page 356.

---
+++ Ch11/starsrch.c
@@ -0,0 +1,27 @@
+// || Found: astronomy
+// || Found: astrophysics
+// || Found: astrophobia
+// || The list contained 3 words beginning with astro.
+// || [Finished in 0 seconds]
+
+#include <stdio.h>
+#include <string.h>
+#define LISTSIZE 6
+
+int main(void) {
+    const char* list[LISTSIZE] = {"astronomy", "astounding", "astrophysics",
+                                  "ostracize", "asterism",   "astrophobia"};
+    int count = 0;
+
+    for (int i = 0; i < LISTSIZE; ++i) {
+        if (strncmp(list[i], "astro", 5) == 0) {
+            printf("Found: %s\n", list[i]);
+            count++;
+        }
+    }
+    printf(
+        "The list contained %d words beginning"
+        " with astro.\n",
+        count);
+    return 0;
+}
