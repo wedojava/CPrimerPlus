252e8e9dd87547ce490022c2dc75a2a6f8c2d46e
wedojava <wedojava@gmail.com>
2020-04-23 00:20:18 +0800

    study done. exercise is the next.

---
+++ Ch11/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+char* pr(char* str);
+
+int main(void) {
+    pr("Ho Ho Ho!");
+
+    return 0;
+}
+
+char* pr(char* str) {
+    char* pc;
+
+    pc = str;
+    while (*pc) {
+        putchar(*pc++);
+    }
+    do {
+        putchar(*--pc);
+    } while (pc - str);
+
+    return pc;
+}
